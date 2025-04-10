1fec01982763f8102eb3468e89dcd16b93069b8d
khalil <khalil@rey.com>
2025-04-10 12:01:14 +0330

    Add project files.

---
+++ WorkShiftAnalyzer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorkShiftAnalyzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
